7cb08008c6570f49a82da09567fcf52e40c78ce1
DivineHoBo <16551410+DivineHoBo@users.noreply.github.com>
2026-05-12 11:15:44 -0400

    Made it so geras forms mirrored the players skin color

---
+++ Content.Client/_Blimpuf/Geras/ClientGerasSystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Shared._Blimpuf.Geras;
+using Robust.Client.GameObjects;
+
+namespace Content.Client._Blimpuf.Geras;
+public sealed class ClientGerasSystem : EntitySystem
+{
+    [Dependency] private readonly SpriteSystem _sprite = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<GerasColorComponent, AfterAutoHandleStateEvent>(OnUpdateColor);
+    }
+    
+    private void OnUpdateColor(EntityUid uid, GerasColorComponent comp, ref AfterAutoHandleStateEvent args)
+    {
+        if (TryComp<SpriteComponent>(uid, out var sprite))
+        {
+            _sprite.LayerSetColor(uid, 0, comp.Color);
+        }
+    }
+    
+}
--- Content.Server/_Blimpuf/Geras/GerasSystem.cs
+++ Content.Server/_Blimpuf/Geras/GerasSystem.cs
@@ -2,6 +2,7 @@
 using Content.Server.Polymorph.Systems;
 using Content.Server.Popups;
 using Content.Shared._Blimpuf.Geras;
+using Content.Shared.Humanoid;
 using Robust.Shared.Player;
 
 namespace Content.Server._Blimpuf.Geras;
@@ -30,7 +31,15 @@ public sealed class GerasSystem : SharedGerasSystem
 
         if (!ent.HasValue)
             return;
-
+        
+        if (!EntityManager.TryGetComponent<HumanoidAppearanceComponent>(uid, out var appearance))
+            return;
+        
+        var gerasColorComponent = EntityManager.EnsureComponent<GerasColorComponent>(ent.Value);
+        gerasColorComponent.Color = appearance.SkinColor;
+        Dirty(ent.Value, gerasColorComponent); 
+        
+        
         _popup.PopupEntity(Loc.GetString("geras-popup-morph-message-others", ("entity", ent.Value)), ent.Value, Filter.PvsExcept(ent.Value), true);
         _popup.PopupEntity(Loc.GetString("geras-popup-morph-message-user"), ent.Value, ent.Value);
     }
