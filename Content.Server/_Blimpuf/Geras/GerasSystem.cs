e76a463ee7ee0458aeb2c96b9788ecdd56a65bf3
DivineHoBo <16551410+DivineHoBo@users.noreply.github.com>
2026-05-14 02:15:34 -0400

    Added DoAfter for the geras ability.

--- Content.Server/_Blimpuf/Geras/GerasSystem.cs
+++ Content.Server/_Blimpuf/Geras/GerasSystem.cs
@@ -2,6 +2,8 @@
 using Content.Server.Polymorph.Systems;
 using Content.Server.Popups;
 using Content.Shared._Blimpuf.Geras;
+using Content.Shared._Blimpuf.Geras.Components;
+using Content.Shared.DoAfter;
 using Content.Shared.Humanoid;
 using Robust.Shared.Player;
 
@@ -12,6 +14,7 @@ public sealed class GerasSystem : SharedGerasSystem
 {
     [Dependency] private readonly ActionsSystem _actionsSystem = default!;
     [Dependency] private readonly PolymorphSystem _polymorphSystem = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
     [Dependency] private PopupSystem _popup = default!;
 
     /// <inheritdoc/>
@@ -19,6 +22,7 @@ public sealed class GerasSystem : SharedGerasSystem
     {
         SubscribeLocalEvent<GerasComponent, MorphIntoGeras>(OnMorphIntoGeras);
         SubscribeLocalEvent<GerasComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<GerasComponent, GerasAbilityDoAfterEvent>(OnDoAfter);
     }
 
     private void OnMapInit(EntityUid uid, GerasComponent component, MapInitEvent args) =>
@@ -27,6 +31,22 @@ public sealed class GerasSystem : SharedGerasSystem
 
     private void OnMorphIntoGeras(EntityUid uid, GerasComponent component, MorphIntoGeras args)
     {
+        var @event = new GerasAbilityDoAfterEvent();
+        // time it takes to activate ability: TimeSpan.FromSeconds(X) X = number of seconds
+        var doAfter = new DoAfterArgs(EntityManager, uid, TimeSpan.FromSeconds(6), @event, uid)
+        {
+            BreakOnDamage = true,
+            BreakOnMove = true
+        };
+        _doAfter.TryStartDoAfter(doAfter);
+    }
+    
+    private void OnDoAfter(EntityUid uid, GerasComponent component, GerasAbilityDoAfterEvent args)
+    {
+        // Check if the event was cancelled or interrupted (e.g., moved while casting)
+        if (args.Cancelled || args.Handled)
+            return;
+        
         var ent = _polymorphSystem.PolymorphEntity(uid, component.GerasPolymorphId);
 
         if (!ent.HasValue)
@@ -42,5 +62,7 @@ public sealed class GerasSystem : SharedGerasSystem
         
         _popup.PopupEntity(Loc.GetString("geras-popup-morph-message-others", ("entity", ent.Value)), ent.Value, Filter.PvsExcept(ent.Value), true);
         _popup.PopupEntity(Loc.GetString("geras-popup-morph-message-user"), ent.Value, ent.Value);
+
+        args.Handled = true;
     }
 }
---
+++ Content.Shared/_Blimpuf/Geras/Components/GerasAbilityDoAfter.cs
@@ -0,0 +1,9 @@
+﻿using Content.Shared.DoAfter;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Blimpuf.Geras.Components;
+
+[Serializable, NetSerializable]
+public sealed partial class GerasAbilityDoAfterEvent : SimpleDoAfterEvent
+{
+}
