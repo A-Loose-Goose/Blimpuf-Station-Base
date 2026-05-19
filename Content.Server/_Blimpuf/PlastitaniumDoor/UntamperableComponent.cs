311e6a62156097716e28932743033e76a4307a35
DivineHoBo <16551410+DivineHoBo@users.noreply.github.com>
2026-05-19 11:49:36 -0400

    Added Plastitanium Door

---
+++ Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Content.Server._Blimpuf.PlastitaniumDoor
+{
+    /// <summary>
+    /// Prevents emagging, hacking, wire-cutting, and RCD deconstruction on this door.
+    /// </summary>
+    [RegisterComponent]
+    public sealed partial class UntamperableComponent : Component
+    {
+    }
+}
