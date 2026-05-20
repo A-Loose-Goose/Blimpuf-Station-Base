22f585e880cbc145acc5beea5207479c95329b75
DivineHoBo <16551410+DivineHoBo@users.noreply.github.com>
2026-05-20 06:55:20 -0400

    Plastitanium Door Update

--- Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableComponent.cs
+++ Content.Server/_Blimpuf/PlastitaniumDoor/UntamperableComponent.cs
@@ -1,7 +1,12 @@
-﻿namespace Content.Server._Blimpuf.PlastitaniumDoor
+﻿using Robust.Shared.Audio;
+
+namespace Content.Server._Blimpuf.PlastitaniumDoor
 {
     [RegisterComponent]
     public sealed partial class UntamperableComponent : Component
     {
+        [DataField("Disabled")] public bool AccessChangeDisabled = false;
+
+        [DataField("denyChangeSound")] public SoundSpecifier DenyChangeSound = new SoundPathSpecifier("/Audio/Machines/airlock_deny.ogg");
     }
 }
