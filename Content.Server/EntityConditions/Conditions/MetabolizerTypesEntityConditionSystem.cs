49abe2852b82afd4d6de676bf1015a037c91dcaa
Stella <stella.middleton@hotmail.com>
2026-07-29 16:39:39 -0400

    Simplified Dipsomania trait implementation

--- Content.Server/EntityConditions/Conditions/MetabolizerTypesEntityConditionSystem.cs
+++ Content.Server/EntityConditions/Conditions/MetabolizerTypesEntityConditionSystem.cs
@@ -1,11 +1,6 @@
 using Content.Shared.Body.Components;
-using Content.Shared.Body.Organ;
-using Content.Shared._Blimpuf.Medical.Body.Components;
-using Content.Shared._Starlight.Medical.Body.Prototypes;
 using Content.Shared.EntityConditions;
 using Content.Shared.EntityConditions.Conditions.Body;
-using Content.Shared.Body.Systems;
-using Robust.Shared.Prototypes;
 
 namespace Content.Server.EntityConditions.Conditions;
 
@@ -15,23 +10,8 @@ namespace Content.Server.EntityConditions.Conditions;
 /// <inheritdoc cref="EntityConditionSystem{T, TCondition}"/>
 public sealed partial class MetabolizerTypeEntityConditionSystem : EntityConditionSystem<MetabolizerComponent, MetabolizerTypeCondition>
 {
-    [Dependency] private SharedBodySystem _body = default!;
-    
-    private static readonly ProtoId<MetabolizerTypePrototype> LegacyDwarfMetabolizerType = "Dwarf";
-
     protected override void Condition(Entity<MetabolizerComponent> entity, ref EntityConditionEvent<MetabolizerTypeCondition> args)
     {
-        if (entity.Comp.MetabolizerTypes?.Overlaps(args.Condition.Type) == true)
-        {
-            args.Result = true;
-            return;
-        }
-
-        if (!args.Condition.Type.Contains(LegacyDwarfMetabolizerType)
-            || !TryComp<OrganComponent>(entity, out var organ)
-            || organ.Body is not { } body)
-            return;
-
-        args.Result = _body.TryGetOrgansWithComponent<DipsomaniaCarrierComponent>((body, null), out _);
+        args.Result = entity.Comp.MetabolizerTypes?.Overlaps(args.Condition.Type) == true;
     }
 }
--- Content.Server/_Blimpuf/Medical/Body/Systems/DipsomaniaSystem.cs
+++ Content.Server/_Blimpuf/Medical/Body/Systems/DipsomaniaSystem.cs
@@ -1,12 +1,16 @@
 using Content.Shared.Body.Components;
 using Content.Shared._Blimpuf.Medical.Body.Components;
 using Content.Shared.Body.Systems;
+using Content.Server._Starlight.Medical.Body.Systems;
 
 namespace Content.Server._Blimpuf.Medical.Body.Systems;
 
 public sealed partial class DipsomaniaSystem : EntitySystem
 {
+    private const string DipsomaniaMetabolizerType = "Dipsomania";
+
     [Dependency] private SharedBodySystem _body = default!;
+    [Dependency] private MetabolizerSystem _metabolizer = default!;
 
     public override void Initialize()
     {
@@ -19,7 +23,12 @@ public sealed partial class DipsomaniaSystem : EntitySystem
             || !_body.TryGetOrgansWithComponent<StomachComponent>((ent.Owner, body), out var stomachs))
             return;
 
-        EnsureComp<DipsomaniaCarrierComponent>(stomachs[0]);
+        var stomach = stomachs[0];
+        if (!TryComp<MetabolizerComponent>(stomach, out var metabolizer))
+            return;
+
+        _metabolizer.TryAddMetabolizerType((stomach, metabolizer), DipsomaniaMetabolizerType);
+        EnsureComp<DipsomaniaCarrierComponent>(stomach);
         RemComp<DipsomaniaComponent>(ent);
     }
 }
