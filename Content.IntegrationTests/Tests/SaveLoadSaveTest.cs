banumbas/space-station-classic

Gametests p2 (#5073)
## Short description
Finishes work started by (Someone) to merge the new test framework from
upstream to starlight.

This just merges
https://github.com/space-wizards/space-station-14/pull/43207 and then
updates starlight tests to bring them into line with the new framework

## Why we need to add this
Because they are quick holy moly.

## Checks
<!-- check boxes for faster reviewing of your PR -->

- [X] I do not require assistance to complete the PR.
- [X] Before posting/requesting review of a PR, I have verified that the
changes work.
- [X] I have added screenshots/videos of the changes, or this PR does
not change in-game mechanics.
- [X] I affirm that my changes are licensed under the [MIT
License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
and grant permission for use in this repository under its conditions.

---------

Co-authored-by: Moony <moony@hellomouse.net>
Co-authored-by: Red Mushie <82113471+redmushie@users.noreply.github.com>

Content.IntegrationTests/Tests/SaveLoadSaveTest.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Linq;
+using Content.IntegrationTests.Fixtures;
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.ContentPack;
@@ -16,12 +17,12 @@ namespace Content.IntegrationTests.Tests
     ///     Tests that a grid's yaml does not change when saved consecutively.
     /// </summary>
     [TestFixture]
-    public sealed class SaveLoadSaveTest
+    public sealed class SaveLoadSaveTest : GameTest
     {
         [Test]
         public async Task CreateSaveLoadSaveGrid()
         {
-            await using var pair = await PoolManager.GetServerClient();
+            var pair = Pair;
             var server = pair.Server;
             var entManager = server.ResolveDependency<IEntityManager>();
             var mapLoader = entManager.System<MapLoaderSystem>();
@@ -85,10 +86,9 @@ namespace Content.IntegrationTests.Tests
                 }
             });
             testSystem.Enabled = false;
-            await pair.CleanReturnAsync();
         }
 
-        private const string TestMap = "Maps/_Starlight/Stations/Saltern.yml"; //Starlight path instead of upstream path
+        private new const string TestMap = "Maps/_Starlight/Stations/Saltern.yml"; //Starlight path instead of upstream path
 
         /// <summary>
         ///     Loads the default map, runs it for 5 ticks, then assert that it did not change.
@@ -96,7 +96,7 @@ namespace Content.IntegrationTests.Tests
         [Test]
         public async Task LoadSaveTicksSave() //Starlight, test renamed as it referenced upstream map in the test name
         {
-            await using var pair = await PoolManager.GetServerClient();
+            var pair = Pair;
             var server = pair.Server;
             var mapLoader = server.ResolveDependency<IEntitySystemManager>().GetEntitySystem<MapLoaderSystem>();
             var mapSys = server.System<SharedMapSystem>();
@@ -167,7 +167,6 @@ namespace Content.IntegrationTests.Tests
 
             testSystem.Enabled = false;
             await server.WaitPost(() => mapSys.DeleteMap(mapId));
-            await pair.CleanReturnAsync();
         }
 
         /// <summary>
@@ -183,7 +182,7 @@ namespace Content.IntegrationTests.Tests
         [Test]
         public async Task LoadTickLoad() //Starlight, test renamed as it referenced upstream map in the test name
         {
-            await using var pair = await PoolManager.GetServerClient();
+            var pair = Pair;
             var server = pair.Server;
 
             var mapLoader = server.System<MapLoaderSystem>();
@@ -241,7 +240,6 @@ namespace Content.IntegrationTests.Tests
             testSystem.Enabled = false;
             await server.WaitPost(() => mapSys.DeleteMap(mapId1));
             await server.WaitPost(() => mapSys.DeleteMap(mapId2));
-            await pair.CleanReturnAsync();
         }
 
         /// <summary>

A-Loose-Goose/Blimpuf-Station-Base

Content.IntegrationTests/Tests/SaveLoadSaveTest.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Linq;
+using Content.IntegrationTests.Fixtures;
 using Content.Shared.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.ContentPack;
@@ -16,12 +17,12 @@ namespace Content.IntegrationTests.Tests
     ///     Tests that a grid's yaml does not change when saved consecutively.
     /// </summary>
     [TestFixture]
-    public sealed class SaveLoadSaveTest
+    public sealed class SaveLoadSaveTest : GameTest
     {
         [Test]
         public async Task CreateSaveLoadSaveGrid()
         {
-            await using var pair = await PoolManager.GetServerClient();
+            var pair = Pair;
             var server = pair.Server;
             var entManager = server.ResolveDependency<IEntityManager>();
             var mapLoader = entManager.System<MapLoaderSystem>();
@@ -85,10 +86,9 @@ namespace Content.IntegrationTests.Tests
                 }
             });
             testSystem.Enabled = false;
-            await pair.CleanReturnAsync();
         }
 
-        private const string TestMap = "Maps/_Blimpuf/Stations/Saltern.yml"; //Starlight path instead of upstream path
+        private new const string TestMap = "Maps/_Blimpuf/Stations/Saltern.yml"; //Blimpuf path instead of upstream path
 
         /// <summary>
         ///     Loads the default map, runs it for 5 ticks, then assert that it did not change.
@@ -96,7 +96,7 @@ namespace Content.IntegrationTests.Tests
         [Test]
         public async Task LoadSaveTicksSave() //Starlight, test renamed as it referenced upstream map in the test name
         {
-            await using var pair = await PoolManager.GetServerClient();
+            var pair = Pair;
             var server = pair.Server;
             var mapLoader = server.ResolveDependency<IEntitySystemManager>().GetEntitySystem<MapLoaderSystem>();
             var mapSys = server.System<SharedMapSystem>();
@@ -167,7 +167,6 @@ namespace Content.IntegrationTests.Tests
 
             testSystem.Enabled = false;
             await server.WaitPost(() => mapSys.DeleteMap(mapId));
-            await pair.CleanReturnAsync();
         }
 
         /// <summary>
@@ -183,7 +182,7 @@ namespace Content.IntegrationTests.Tests
         [Test]
         public async Task LoadTickLoad() //Starlight, test renamed as it referenced upstream map in the test name
         {
-            await using var pair = await PoolManager.GetServerClient();
+            var pair = Pair;
             var server = pair.Server;
 
             var mapLoader = server.System<MapLoaderSystem>();
@@ -241,7 +240,6 @@ namespace Content.IntegrationTests.Tests
             testSystem.Enabled = false;
             await server.WaitPost(() => mapSys.DeleteMap(mapId1));
             await server.WaitPost(() => mapSys.DeleteMap(mapId2));
-            await pair.CleanReturnAsync();
         }
 
         /// <summary>