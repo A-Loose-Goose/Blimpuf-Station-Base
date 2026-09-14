a92998987f6ff5c87a35697ef48181975b1b20f7
ALooseGoose <ALooseGoosey@gmail.com>
2026-09-14 19:33:49 +0100

    Antag Briefing fix for replays

---
+++ Content.Shared/Antag/SharedAntagBriefingSoundSystem.cs
@@ -0,0 +1,16 @@
+using Robust.Shared.Audio;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Antag;
+
+[Serializable, NetSerializable]
+public sealed class AntagBriefingSoundEvent : EntityEventArgs
+{
+    public SoundSpecifier Sound { get; }
+
+    public AntagBriefingSoundEvent(SoundSpecifier sound)
+    {
+        Sound = sound;
+    }
+}
